fcadce0d9af4c4029ba8bdd3ed6bf3ebd6aac257
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-17 13:55:17 +0900

    connect to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"flag"
 	"log"
-	"net/http"
 )
 
 const port = 8080
 
 type application struct {
 	Domain string
-
+	DSN string // data string name
+	DB *sql.DB
 }
 
 func main() {
@@ -18,19 +19,29 @@ func main() {
 	// 宣言のみ。　代入なし
 	var app application // application is the type that is declared above
 
-	// read from command line
+	// // read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "postgres connection string")
+	flag.Parse()
 
 	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
+	app.Domain = "example.com"
 
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
-	// start a web server
-	// 宣言と代入を一緒にするパターン (var省略)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	// // start a web server
+	// // 宣言と代入を一緒にするパターン (var省略)
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
