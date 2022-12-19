fd6175db4c8d3c89a2ff4426c9fe0677cb02beb4
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-19 15:05:50 +0900

    implemetnt readJson function

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"reflect"
 )
@@ -46,5 +48,23 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 		return err
 	}
 
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1024 * 1024 // one megabyte
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	dec := json.NewDecoder(r.Body)
+
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF { // end of the file
+		return errors.New("body must only contain a single json")
+	}
 	return nil
 }
