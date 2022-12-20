00b6b7b6c406ac0d15e0536eaf46245c044ba067
Yoshihisa19920807 <yoshi19920807@gmail.com>
2022-12-20 16:06:32 +0900

    implement GetuserByEmail

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -20,7 +20,6 @@ func (m *PostgresDBRepo) Connection() *sql.DB {
 
 func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
-
 	defer cancel()
 
 	query := `
@@ -64,4 +63,32 @@ func (m *PostgresDBRepo) AllMovies() ([]*models.Movie, error) {
 	}
 
 	return movies, nil
+}
+
+func (m *PostgresDBRepo) GetUserByEmail(email string) (*models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password,
+	create_at, updated_at, from users where email = $1`
+	var user models.User
+	row := m.DB.QueryRowContext(ctx, query, email)
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	println(user.ID)
+	println(&user.ID)
+
+	return &user, nil // &<var>でpointerアドレスを返す
 }
